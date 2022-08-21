71ae48b148ccf854b2c7a5f36a530cc89cf567f2
Will Sargent <will.sargent@gmail.com>
2022-08-21 09:38:37 -0700

    scalafix

--- rules/src/main/scala/fix/EchopraxiaScalafix.scala
+++ rules/src/main/scala/fix/EchopraxiaScalafix.scala
@@ -5,19 +5,34 @@ import scala.meta._
 
 class EchopraxiaScalafix extends SemanticRule("EchopraxiaScalafix") {
 
+  private val loggerType = SymbolMatcher.normalized("com.tersesystems.echopraxia.plusscala.flow.FlowLogger")
+  private val loggerName = "flowLogger"
+  private val loggerMethod = "trace"
+
   override def fix(implicit doc: SemanticDocument): Patch = {
 
     doc.tree.collect {
-      case Defn.Def(mods, name, tparams, paramss, decltpe, body) =>
+      case Defn.Def(_, _, _, _, _, body) =>
         body match {
-          case Term.Apply(Term.Select(Term.Name("flowLogger"), Term.Name("trace")), _) =>
+          case Term.Apply(Term.Select(name, _), _) if matchesType(name) =>
+            // Is "flowLogger.trace" already existing?  Then don't add it again.
+            // Technically this looks for a matching symbol so you can have a different name on the logger.
             Patch.empty
           case other =>
-            println("body.structure: " + other.structure)
-            Patch.addLeft (body, "flowLogger.trace ")
+            Patch.addLeft (body, s"$loggerName.$loggerMethod ")
         }
 
     }.asPatch
   }
+
+  private def matchesType(name: Term)(implicit doc: SemanticDocument): Boolean = {
+    doc.info(name.symbol).collect { info =>
+      info.signature match {
+        case MethodSignature(_, _, TypeRef(_, symbol, _)) if loggerType.matches(symbol) =>
+          true
+      }
+    }.getOrElse(false)
+  }
+
 
 }
